9363bde0c0b05d24593b2f09b7d080ccb5bfaf3c
AIN1995 <abe19950311@gmail.com>
2022-07-05 22:01:31 +0900

    todo

---
+++ todo/Js.js
@@ -0,0 +1,10 @@
+const button = document.getElementById("touroku");
+const input = document.getElementById("yarukoto");
+
+button.addEventListener("click",() =>{
+    const li = dcoument.createElement("li");
+    li.innertext = input.value;
+
+   
+    
+});
