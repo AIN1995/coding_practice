a7665909c8cfe60e750b7d1de6f49e5537d7886a
AIN1995 <abe19950311@gmail.com>
2022-07-08 21:55:50 +0900

    7/8

---
+++ 0708模写/index.js
@@ -0,0 +1,7 @@
+$(function() {          //jqueryのコード記述　$(document).ready(function()) {　の簡略表示
+    $("#msg").css("font-size","30px");      //jqueryのコード記述
+});
+
+$(function() {
+    $("#parent > p").css("font-size","30px"); //id parentの子要素pに適用
+});
