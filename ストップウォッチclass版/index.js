c9c92d808e9919973de686db56ca3f34521b8f28
AIN1995 <abe19950311@gmail.com>
2022-07-09 21:20:10 +0900

    0709

---
+++ ストップウォッチclass版/index.js
@@ -0,0 +1,56 @@
+class StopWatch{
+    constructor(option={}){
+        this.option = option;
+    }
+
+    init(){
+        const startButton = document.getElementsByClassName("startButton")[0];
+        const display = document.getElementsByClassName("display")[0];
+        const stopButton = document.getElementsByClassName("stopButton")[0];
+        const log = document.getElementsByClassName("log")[0];
+        let timer = null;
+        let {color,backgroundColor} = this.option;
+
+        color = color || "lightblue";
+        backgroundColor = backgroundColor || "black";
+
+        display.style.color = color;
+        display.style.backgroundColor = backgroundColor;
+
+        startButton.addEventListener("click",function(){
+            let seconds = 0;
+            if(timer===null){
+            timer = setInterval(function(){
+                seconds++;
+                display.innerText = seconds;
+            },1000);
+            addMessager("開始");
+            }
+        });
+
+        stopButton.addEventListener("click",function(){
+            if(timer!==null){
+            clearInterval(timer);
+            timer = null;
+            addMessager("終了");
+            }
+        });
+
+        function addMessager(message){
+            const logtext = document.createElement("div");
+            const now = new Date();
+            logtext.classList = ["message"];
+            logtext.innerText = `${now.getHours()}時${now.getMinutes()}分${now.getSeconds()}秒 ${message}`;
+            log.appendChild(logtext);
+        }
+        
+    }
+}
+
+const option  = {
+    color:"limegreen",
+    backgroundColor:"#333"
+}
+
+const stopWatch = new StopWatch(option);
+stopWatch.init();
