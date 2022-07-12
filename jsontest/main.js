4835687e0ee21f96061def53c1d244c8332b66ab
AIN1995 <abe19950311@gmail.com>
2022-07-12 20:53:39 +0900

    更新

--- jsontest/main.js
+++ jsontest/main.js
@@ -16,4 +16,47 @@ class AreaSelector {
         return await prefResponse.json();
     }
 
+    async getCities(prefCode) {
+        const cityResponse = await fetch(`./cities/${prefCode}.json`);
+        return await cityResponse.json();
+    }
+
+    async updatePref() {
+        this.prefectures = await this.getPrefs();
+        this.prefCode = this.prefectures[0].code;
+        this.createPrefOptionHtml();
+    }
+
+    async updateCity() {
+        this.cities = await this.getCities(this.prefCode);
+        this.createCityOptionHtml();
+    }
+
+    createPrefOptionHtml() {
+        const prefSelctorElm = this.rootElm.querySelector(".prefectures");
+        prefSelctorElm.innerHTML = this.toOptionsHtml(this.prefectures);
+
+        prefSelctorElm.addEventListener("change",(event)=>{
+            this.prefCode = event.target.value;
+            this.updateCity();
+        });
+    }
+
+    createCityOptionHtml() {
+        const citySelectorElm = this.rootElm.querySelector(".cities");
+        citySelectorElm.innerHTML = this.toOptionsHtml(this.cities);
+    }
+
+    toOptionsHtml(records) {
+        return records.map((record)=>{
+            return`
+            <option name="${record.name}" value="${record.code}">
+            ${record.name}
+            </option>
+            `;
+        }).join("");
+    } 
 }
+
+const areaSelector = new AreaSelector(document.getElementById("areaSelector"));
+areaSelector.init();
