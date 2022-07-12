2fdb135c870912125449ece4a00982aad993823c
AIN1995 <abe19950311@gmail.com>
2022-07-12 16:49:15 +0900

    class化

--- todoリスト/index.js
+++ todoリスト/index.js
@@ -1,8 +1,5 @@
 class todoList {
 
-  constructor() {
-  }
-
   init() {
     const form = document.getElementById("form");
     const input = form.querySelector(".form-control");
@@ -25,15 +22,24 @@ class todoList {
     forminput(list) {
       let inputtext = document.createElement("li");
       if(list) {
-        inputtext.innerText = list;
+        inputtext.innerText = list.tag;
       }else {
         inputtext.innerText = input.value;
       }
+
+      if(list && list.class) {
+        inputtext.classList.add("text-decoration-line-through");
+      }
+
       ul.appendChild(inputtext);
 
       inputtext.addEventListener("contextmenu",(event)=>{
         event.preventDefault();
         this.deleteli(inputtext);
+      });
+
+      inputtext.addEventListener("click",()=>{
+        this.border(inputtext);
       });
 
       this.saveData();
@@ -44,19 +50,25 @@ class todoList {
       this.saveData();
     }
 
+    border(inputtext) {
+      inputtext.classList.toggle("text-decoration-line-through");
+      this.saveData();
+    }
 
     saveData() {
       let save = document.querySelectorAll("li");
       let lists = [];
       for(let list of save) {
-        lists.push(list.innerText);
+          let taglist = {
+            tag:list.innerText,
+            class:list.classList.contains("text-decoration-line-through")
+          }
+        lists.push(taglist);
       }
      localStorage.setItem("list",JSON.stringify(lists));
     }
 
-
  }
-
 
 const todolist = new todoList().init();
 
