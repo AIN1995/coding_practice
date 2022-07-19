cb455852e406c8f592c8ed6172059e0690f3a142
AIN1995 <abe19950311@gmail.com>
2022-07-19 18:42:28 +0900

    0719

---
+++ typescript_test/index.ts
@@ -0,0 +1,12 @@
+let nametest ="test";
+let total = 12;
+let show  = true;
+let namelist = ["1","2","3"];
+let namberlist = [1,3,4];
+let person = {
+    name:"list",
+    age:12
+}
+let sayhello = (name:string):void=>{
+
+}
